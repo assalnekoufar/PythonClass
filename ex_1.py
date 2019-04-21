e26ab34dcaa5935499eda6ea23dc630b43b65012
asaln <asalnekoofar@yahoo.com>
2019-04-21 14:31:46 +0430

    My answer to example one.

---
+++ ex_1.py
@@ -0,0 +1,12 @@
+height = float(input("what is your height(m) ? --->"))
+weight = float(input("what is your weight(kg) ? --->"))
+BMI = height/(weight**2)
+
+if BMI < 18.5:
+    print("underweight")
+elif 18.5 <= BMI <= 25:
+    print("normal")
+elif 25 <= BMI <= 30:
+    print("overweight")
+else:
+     print("obesity")
