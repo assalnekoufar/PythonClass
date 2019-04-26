019e3864fb318ef9834b0fc035dfad2d1b3d2b08
asaln <asalnekoofar@yahoo.com>
2019-04-26 15:49:08 +0430

    Second try for solving ex2_1_1!

--- ex2_1.py
+++ ex2_1.py
@@ -1,5 +1,15 @@
-## < taraf samte chap istade va > yani taraf samte rast istade!
-new_state = []
-state_1 = "<<<<><<<<><<<<"
-new_state = state_1.split("><")
-print(new_state)
+## < taraf smte chap istade, > taraf samte rast istade!
+state_1 = "<<<><<<<><<<<><<<<"
+stage_2 = ""
+for i in state_1:
+    if state_1 == ">":
+        state_1 = "<"
+    stage_2 += state_1
+    print(stage_2)
+else:
+    stage_2 += state_1
+print(stage_2)
+
+
+
+
