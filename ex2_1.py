172ef618112636a5e9c30e89241f7d64d2547890
asaln <asalnekoofar@yahoo.com>
2019-04-26 17:37:01 +0430

    My forth try for solving ex2_1_1 :))!

--- ex2_1.py
+++ ex2_1.py
@@ -1,12 +1,14 @@
-## < samte chap , > samte rast, natije : <<><<><
+# ## < samte chap , > samte rast, natije : <<><<><
 state_1 = "<><<><<"
+state_1_list = []
 lst = []
 i = -1
-for c in state_1:
-    lst.append(c)
+for w in state_1:
+    state_1_list.append(w)
+for c in state_1_list:
+    lst += c
     i += 1
-    if i > 0 and lst[i-1] == ">" and lst[i] == "<":
+    if i > 0 and state_1_list[i-1] == ">" and state_1_list[i] == "<":
         lst[i-1] = "<"
         lst[i] = ">"
-
-print(lst[:])
+print(lst)
