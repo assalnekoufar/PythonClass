ee9e452eed61ec2bd9dd07152934114c9f5d4a83
asaln <assal.nekoufar@gmail.com>
2019-05-04 08:13:28 -0700

    let's filter our prime list!

--- prime_number.py
+++ prime_number.py
@@ -1,8 +1,16 @@
+import random
+import math
+
+
 def find_prime_number(n):
-    for i in range(2, n):
+    for i in range(2, int(math.sqrt(n))):
         if n % i == 0:
             return "is not a prime number"
         return "is a prime number"
 
 
-print(find_prime_number(3))
+lst_1 = [random.randint(2, 10000) for i in range(1, 1000)]
+
+primes = [n1 for n1 in lst_1 if find_prime_number(n1) == "is a prime number"]
+print(primes)
+
